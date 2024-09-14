e3608bd09e942e5a9737565f2f3b71211cff5e55
AzizulHakimArif <User@DESKTOP-0UBQBM5>
2024-09-14 11:11:30 +0600

    Add project files.

---
+++ EmployeeManagement/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EmployeeManagementSystem.Models;
+
+namespace EmployeeManagementSystem.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ EmployeeManagement/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeeManagementSystem.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PhotoPath { get; set; }
+    }
+}
